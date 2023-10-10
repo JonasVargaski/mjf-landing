eb4a2204316ddd3f0bc484935f6a5b2b1809161e
Jonas <jonasvargaski@hotmail.com>
2023-10-10 18:33:36 -0300

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { RootProviders } from './providers'
 import { WhatsAppFloat } from '@/components/ui/whatsapp-float'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -39,6 +40,8 @@ export const metadata: Metadata = {
   },
 }
 
+const GTM_ID = 'GTM-M2DTSR6L'
+
 export default function RootLayout({
   children,
 }: {
@@ -46,6 +49,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
       <body className={inter.className}>
         <RootProviders>{children}</RootProviders>
         <WhatsAppFloat />
